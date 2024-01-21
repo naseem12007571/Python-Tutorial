d72ea782edfcfadabc3d1bc5afbb3cd6b3fe396a
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-01-22 00:23:25 +0530

    Create basice.py

---
+++ basice.py
@@ -0,0 +1,43 @@
+
+print("hello naseem");
+
+# variable in python
+
+a=10;
+print(a);
+b=30;
+print(b);
+
+c=3.4;
+print(c);
+
+# string data type
+c="naseem";
+print(c);
+
+# this is string type
+f="5";    
+print (int (f)+1)
+
+# and this is a int type 
+d=5
+print(d+1)
+
+# operator 
+
+nums1=30;
+nums2=20;
+print("nums1 + nums2 = " , nums1+nums2);
+print("nums1 + nums2 = " , nums1-nums2);
+print("nums1 + nums2 = " , nums1*nums2);
+print("nums1 + nums2 = " , nums1/nums2);
+
+# assignment operator
+a=4
+a=a+1;
+print(a);
+
+
+
+
+
