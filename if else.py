90f0a7212ac4d5ac9d68bda235eae0c11257bbed
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-01-25 23:47:29 +0530

    Create if else.py

---
+++ if else.py
@@ -0,0 +1,50 @@
+
+# number = int(input("Enter the value"))
+# if number>=0:
+#     print("numer is positive ")
+# else:
+#     print("number is negative")
+
+
+#     # number is odd or even;
+# number =int (input("enter the number"))
+
+# if number % 2 == 0:
+#         print("number is even")
+# else:
+#         print("it is odd ")
+
+
+# inside the three number who is greater
+# n1= int(input("enter the first number"))
+# n2= int(input("enter the second number"))
+# n3= int(input("enter the third number"))
+
+# if  n1>n2 and n1>n3:
+#   print(n1," n1 is the greater")
+# elif n2>n1 and n2>n3:
+#   print(n2," n2 is the greater ")
+# else:
+#   print(n3,"n3 is the greater")
+
+
+num=int(input("Enter the number "))
+
+if num%15==0:
+    print("number is divisiable by 15th")
+else:
+    if num % 3 == 0 or num % 5 ==0:
+        print("num is not divisable by 15")
+    else: 
+        print("number is not diviable by 3 and 5")
+
+
+
+
+
+
+
+
+
+
+
