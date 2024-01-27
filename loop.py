d2c60c5abf223b596c2b34c4ae4a41c6ce406d41
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-01-27 23:35:56 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,27 @@
+# for i in range(1,10):
+#     print(i,"how are you ")
+
+# we can etrate on the list 
+# list = [12,34,56,7,9]
+# for i in list:
+#   print(i)
+  
+# fruit = ["mango","orange","apple"]
+# for i in fruit:
+#     print(i)
+
+#   use while loop for print even numberr
+i=2
+while i <= 100:
+    print(i)
+    i=i+2
+
+    
+
+    
+
+
+
+
+
+
