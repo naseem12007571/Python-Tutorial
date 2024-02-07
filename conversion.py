67cf6a188c7a0e10d6eb8956ac9a6064b82429ac
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-02-07 23:51:17 +0530

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,19 @@
+# type conversion
+a=12
+b=4.33
+sum= a+b
+print(sum)
+# we can do type casting with string 
+c= float("22")
+v=4.23
+sum=c+v
+print (sum)
+
+n= int("6")
+v=4.2
+sum=n+v
+print(type(v))
+print(sum)
+
+
+
