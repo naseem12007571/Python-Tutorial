540ad14cf2003ee12311406ee660f18cb5801b7f
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-02-08 23:01:27 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,24 @@
+# new line \n of use in python
+str = "This is string \nthis is mine string "
+print(str)
+
+# tab use in python 
+str1 = "how are you \twhat are you doing"
+print(str1)
+
+str1= "Naseem"
+str2= "Choudhary"
+print(str1+str2)
+# or we can do like it 
+final_str= str1+ " "+str2
+print(final_str)
+
+#  length of string
+len1= len(final_str)
+print(len1)
+
+print(len(final_str))
+
+
+
+
